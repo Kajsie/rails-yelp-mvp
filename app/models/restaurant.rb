3fb757021e22e1d5ba52eb6357cffc84240280bd
kajsa kristersson <k.kristersson@gmail.com>
2017-08-16 14:59:27 +0200

    controllers added

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,7 @@
 class Restaurant < ApplicationRecord
   has_many :reviews,
   dependent: :destroy
+
+  validates_presence_of :name, :address
+  validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"], allow_nil: false }
 end
